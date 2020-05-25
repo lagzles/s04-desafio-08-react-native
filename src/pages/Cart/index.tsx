58a6400bc8eadf48c07fa0cf6a3e32f01b44a723
Leandro Arns <leandro.arns@gmail.com>
2020-05-25 14:50:22 -0300

    ajuste de total de items e valor total do carrinho

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -47,20 +47,29 @@ const Cart: React.FC = () => {
     decrement(id);
   }
 
-  const cartTotal = useMemo(() => {
-    const priceList: number[] = products.map(product => product.price * product.quantity)
-    const reducer = (accu: number, current: number) => accu + current;
-    const totalPrice = priceList.reduce(reducer);
 
-    return formatValue(totalPrice);
+  const cartTotal = useMemo(() => {
+    try {
+      const priceList: number[] = products.map(product => product.price * product.quantity)
+      const reducer = (accu: number, current: number) => accu + current;
+      const totalPrice = priceList.reduce(reducer);
+
+      return formatValue(totalPrice);
+    } catch (err) {
+      return formatValue(0);
+    }
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    const itemsList: number[] = products.map(product => 1 * product.quantity)
-    const reducer = (accu: number, current: number) => accu + current;
-    const totalItems = itemsList.reduce(reducer);
+    try {
+      const itemsList: number[] = products.map(product => 1 * product.quantity)
+      const reducer = (accu: number, current: number) => accu + current;
+      const totalItems = itemsList.reduce(reducer);
 
-    return totalItems;
+      return totalItems;
+    } catch (err) {
+      return 0;
+    }
   }, [products]);
 
   return (
